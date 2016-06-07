a25eb36b7588f5cdc227064ff21e99dec9bb7c32
Absinthe <Sunconure11@users.noreply.github.com>
2016-06-07 06:34:27 -0400

    attempting to use palaeplanks instead of normal planks, because F&A weirdness

--- src/java/growthcraft/core/integration/fa/EnumFAWoodType.java
+++ src/java/growthcraft/core/integration/fa/EnumFAWoodType.java
@@ -46,7 +46,7 @@ public enum EnumFAWoodType
 
 	public ItemStack asPlanksItemStack(int size)
 	{
-		final Block block = GameRegistry.findBlock(FAPlatform.MOD_ID, "planks");
+		final Block block = GameRegistry.findBlock(FAPlatform.MOD_ID, "palaePlanks");
 		if (block != null)
 		{
 			final ItemStack result = new ItemStack(block, size, meta);
