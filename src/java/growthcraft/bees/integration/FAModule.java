292be8ae8ae1a974b64496b99204f4c357bd844a
Absinthe <Sunconure11@users.noreply.github.com>
2016-06-07 01:00:48 -0400

    Update FAModule.java

--- src/java/growthcraft/bees/integration/FAModule.java
+++ src/java/growthcraft/bees/integration/FAModule.java
@@ -62,7 +62,7 @@ public class FAModule extends ModIntegrationBase
 			final ItemStack planks = type.asPlanksItemStack();
 			if (planks != null)
 			{
-				GameRegistry.addShapedRecipe(GrowthCraftBees.beeBoxBotania.asStack(1, type.meta), " A ", "A A", "AAA", 'A', planks);
+				GameRegistry.addShapedRecipe(GrowthCraftBees.beeBoxFA.asStack(1, type.meta), " A ", "A A", "AAA", 'A', planks);
 			}
 		}
 	}
