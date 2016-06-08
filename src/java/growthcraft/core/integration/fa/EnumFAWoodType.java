8dd8451806e1ccedbb653a30494a9a2b90ede61b
Absinthe <Sunconure11@users.noreply.github.com>
2016-06-08 04:44:42 -0400

    Update EnumFAWoodType.java

--- src/java/growthcraft/core/integration/fa/EnumFAWoodType.java
+++ src/java/growthcraft/core/integration/fa/EnumFAWoodType.java
@@ -46,7 +46,7 @@ public enum EnumFAWoodType
 
 	public ItemStack asPlanksItemStack(int size)
 	{
-		final Block block = GameRegistry.findBlock(FAPlatform.MOD_ID, "BlockPalaePlanks");
+		final Block block = GameRegistry.findBlock("fossils", "BlockPalaePlanks");
 		if (block != null)
 		{
 			final ItemStack result = new ItemStack(block, size, meta);
