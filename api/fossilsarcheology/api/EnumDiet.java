c38b70f573532333672c7ccaa311440e39b48812
Absinthe <Sunconure11@users.noreply.github.com>
2016-06-13 05:56:41 -0400

    Update EnumDiet.java

--- api/fossilsarcheology/api/EnumDiet.java
+++ api/fossilsarcheology/api/EnumDiet.java
@@ -2,25 +2,25 @@ package fossilsarcheology.api;
 
 public enum EnumDiet {
 	/**Eats exclusively meat.*/
-    CARNIVORE(3),
+	CARNIVORE(3),
 	/**Eats exclusively plants.*/
-    HERBIVORE(0),
+	HERBIVORE(0),
 	/**Eats meat,eggs and plants.*/
-    OMNIVORE(1),
+	OMNIVORE(1),
 	/**Eats exclusively fish.*/
-    PISCIVORE(1),
+	PISCIVORE(1),
 	/**Eats meat and eggs.*/
-    CARNIVORE_EGG(2),
+	CARNIVORE_EGG(2),
 	/**Eats arthropods.*/
-    INSECTIVORE(0),
+	INSECTIVORE(0),
 	/**Eats meat and fish.*/
-    PISCCARNIVORE(3),
+	PISCCARNIVORE(3),
 	/**Animal does not eat.*/
-    NONE(0);
+	NONE(0);
 
-    public int fearIndex;
+	public int fearIndex;
 
-    EnumDiet(int fearIndex) {
-        this.fearIndex = fearIndex;
-    }
+	EnumDiet(int fearIndex) {
+		this.fearIndex = fearIndex;
+	}
 }
