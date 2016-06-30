32cfdc2a78baa7db36be1aa04cb855a469441f3f
Sunconure11 <eyespot7b@aol.com>
2016-06-30 08:39:52 -0400

    removed unused imports

--- src/java/growthcraft/nether/init/GrcNetherFluids.java
+++ src/java/growthcraft/nether/init/GrcNetherFluids.java
@@ -36,7 +36,6 @@ import growthcraft.core.common.definition.ItemDefinition;
 import growthcraft.core.common.GrcModuleBase;
 import growthcraft.nether.nether;
 
-import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
