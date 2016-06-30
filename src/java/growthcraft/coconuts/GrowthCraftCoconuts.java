1f21f8c94d2147c9e873fc5bbeebe0867e7bd42c
Sunconure11 <eyespot7b@aol.com>
2016-06-30 06:33:24 -0400

    attempting to create a modlet as a dummy

---
+++ src/java/growthcraft/coconuts/GrowthCraftCoconuts.java
@@ -0,0 +1,20 @@
+package growthcraft.coconuts;
+
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod;
+
+@Mod(
+	modid = GrowthCraftCoconuts.MOD_ID,
+	name = GrowthCraftCoconuts.MOD_NAME,
+	version = GrowthCraftCoconuts.MOD_VERSION,
+	dependencies = "required-after:Growthcraft@@VERSION@;required-after:Growthcraft|Cellar@@VERSION@"
+)
+public class GrowthCraftCoconuts
+{
+	public static final String MOD_ID = "Growthcraft|Coconuts";
+	public static final String MOD_NAME = "Growthcraft Coconuts";
+	public static final String MOD_VERSION = "@VERSION@";
+
+	@Instance(MOD_ID)
+	}
+}
