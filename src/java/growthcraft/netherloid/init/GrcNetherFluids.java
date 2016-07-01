ef05f995e012f3be1e3263a8ac7b05cf674f4e46
Sunconure11 <eyespot7b@aol.com>
2016-07-01 05:44:02 -0400

    Take me away from time and season

--- src/java/growthcraft/netherloid/init/GrcNetherFluids.java
+++ src/java/growthcraft/netherloid/init/GrcNetherFluids.java
@@ -35,7 +35,7 @@ import growthcraft.cellar.util.BoozeRegistryHelper;
 import growthcraft.cellar.common.item.EnumYeast;
 import growthcraft.core.common.definition.ItemDefinition;
 import growthcraft.core.common.GrcModuleBase;
-import growthcraft.netherloid;
+import growthcraft.netherloid.Netherloid;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
