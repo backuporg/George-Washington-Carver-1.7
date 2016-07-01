7ace035c34c2764a0b452acb08381d97d671309e
Sunconure11 <eyespot7b@aol.com>
2016-07-01 07:12:31 -0400

    more wip

--- src/java/growthcraft/netherloid/init/GrcNetherFluids.java
+++ src/java/growthcraft/netherloid/init/GrcNetherFluids.java
@@ -38,6 +38,7 @@ import growthcraft.cellar.common.item.EnumYeast;
 import growthcraft.core.common.definition.ItemDefinition;
 import growthcraft.core.common.GrcModuleBase;
 import growthcraft.netherloid.Netherloid;
+import growthcraft.api.core.item.OreItemStacks;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.init.Items;
