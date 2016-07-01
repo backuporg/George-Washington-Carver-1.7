0df1cc364b20cbff715d0cc30713e2a149972637
Sunconure11 <eyespot7b@aol.com>
2016-06-30 20:29:34 -0400

    more minor work

--- src/java/growthcraft/netherloid/common/block/BlockNetherFungusBase.java
+++ src/java/growthcraft/netherloid/common/block/BlockNetherFungusBase.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package id2h.netherloid.common.block;
+package growthcraft.netherloid.common.block;
 
 import java.util.Random;
 
