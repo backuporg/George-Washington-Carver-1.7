7c4091aadd5fb04bea985d3c2d49c5d4e45e99f7
Sunconure11 <eyespot7b@aol.com>
2016-06-30 20:31:40 -0400

    config

--- src/java/growthcraft/netherloid/GrcNetherConfig.java
+++ src/java/growthcraft/netherloid/GrcNetherConfig.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package id2h.netherloid;
+package growthcraft.netherloid;
 
 import growthcraft.core.ConfigBase;
 
