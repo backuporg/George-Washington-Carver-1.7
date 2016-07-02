97ce8e1f0b58ce67a1285fa26d3ec06a69c0eff3
Sunconure11 <eyespot7b@aol.com>
2016-07-02 04:50:03 -0400

    hopefully this fixes the array index error

--- src/java/growthcraft/netherloid/init/netherloidFluids.java
+++ src/java/growthcraft/netherloid/init/netherloidFluids.java
@@ -65,7 +65,7 @@ public class netherloidFluids extends GrcModuleBase
 	@Override
 	public void preInit()
 	{
-		this.maliceCiderBooze = new Booze[0];
+		this.maliceCiderBooze = new Booze[100];
 		this.maliceCiderFluids = new BlockBoozeDefinition[maliceCiderBooze.length];
 		this.maliceCiderBuckets = new ItemBucketBoozeDefinition[maliceCiderBooze.length];
 		BoozeRegistryHelper.initializeBoozeFluids("grc.maliceCider", maliceCiderBooze);
@@ -79,7 +79,7 @@ public class netherloidFluids extends GrcModuleBase
 
 		this.maliceCider = new ItemDefinition(new ItemBoozeBottle(maliceCiderBooze));
 		
-		this.fireBrandyBooze = new Booze[0];
+		this.fireBrandyBooze = new Booze[100];
 		this.fireBrandyFluids = new BlockBoozeDefinition[fireBrandyBooze.length];
 		this.fireBrandyBuckets = new ItemBucketBoozeDefinition[fireBrandyBooze.length];
 		BoozeRegistryHelper.initializeBoozeFluids("grc.fireBrandy", fireBrandyBooze);
