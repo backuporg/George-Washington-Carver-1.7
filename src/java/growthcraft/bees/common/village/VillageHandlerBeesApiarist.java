92d906893bf3e06307dcaedac8b13cde3d48c13c
Absinthe <Sunconure11@users.noreply.github.com>
2016-08-14 16:18:00 -0400

    Update VillageHandlerBeesApiarist.java

--- src/java/growthcraft/bees/common/village/VillageHandlerBeesApiarist.java
+++ src/java/growthcraft/bees/common/village/VillageHandlerBeesApiarist.java
@@ -29,10 +29,10 @@ public class VillageHandlerBeesApiarist implements IVillageTradeHandler, IVillag
 		recipeList.add(new MerchantRecipe(new ItemStack(Items.emerald, 1 + random.nextInt(2)), GrowthCraftBees.items.honeyCombFilled.asStack(1, 7)));
 		recipeList.add(new MerchantRecipe(new ItemStack(Items.emerald, 1 + random.nextInt(2)), GrowthCraftBees.items.bee.asStack(1, 7)));
 		recipeList.add(new MerchantRecipe(new ItemStack(Items.emerald, 1 + random.nextInt(2)), GrowthCraftBees.items.beesWax.asStack(1, 5)));
-		recipeList.add(new MerchantRecipe(new ItemStack(GrowthCraftBees.items.honeyJar, 4), new ItemStack(Items.emerald, 2, 3)));
-		recipeList.add(new MerchantRecipe(new ItemStack(GrowthCraftBees.items.honeyCombFilled, 4), new ItemStack(Items.emerald, 2, 3)));
-		recipeList.add(new MerchantRecipe(new ItemStack(GrowthCraftBees.items.bee, 4), new ItemStack(Items.emerald, 1, 2)));
-		recipeList.add(new MerchantRecipe(new ItemStack(GrowthCraftBees.items.beesWax, 1), new ItemStack(Items.emerald, 2, 4)));
+		recipeList.add(new MerchantRecipe(new ItemStack(GrowthCraftBees.items.honeyJar, 4, 6), new ItemStack(Items.emerald, 2, 3)));
+		recipeList.add(new MerchantRecipe(new ItemStack(GrowthCraftBees.items.honeyCombFilled, 4, 6), new ItemStack(Items.emerald, 2, 3)));
+		recipeList.add(new MerchantRecipe(new ItemStack(GrowthCraftBees.items.bee, 4, 6), new ItemStack(Items.emerald, 1, 2)));
+		recipeList.add(new MerchantRecipe(new ItemStack(GrowthCraftBees.items.beesWax, 1, 2), new ItemStack(Items.emerald, 2, 4)));
 	}
 
 	@Override
