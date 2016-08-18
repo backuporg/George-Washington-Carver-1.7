0794425de7d1bfbbd5f79b397e4031ecc520c95b
Absinthe <Sunconure11@users.noreply.github.com>
2016-08-17 20:38:38 -0400

    Update GrcMilkBlocks.java

--- src/java/growthcraft/milk/init/GrcMilkBlocks.java
+++ src/java/growthcraft/milk/init/GrcMilkBlocks.java
@@ -90,6 +90,7 @@ public class GrcMilkBlocks extends GrcModuleBase
 		if (thistle != null)
 		{
 			thistle.register("grcmilk.Thistle");
+			Blocks.fire.setFireInfo(thistle.getBlock(), 30, 60);
 		}
 		registerOres();
 	}
