4ddddb48c1c7be1595d23c8854896428d55fd2f6
Sunconure11 <eyespot7b@aol.com>
2016-08-28 14:22:12 -0400

    some fixes

--- src/java/growthcraft/api/core/client/particle/EntityFXDropParticle.java
+++ src/java/growthcraft/api/core/client/particle/EntityFXDropParticle.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.particle.Particle;
-import net.minecraft.util.MathHelper;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.IFluidBlock;
 
