d9f70ddb401ab9d35aba937047ef25ca75098c63
Sunconure11 <eyespot7b@aol.com>
2016-08-28 16:13:08 -0400

    toying around with stuff

--- src/java/growthcraft/api/cellar/heatsource/user/UserHeatSourcesConfig.java
+++ src/java/growthcraft/api/cellar/heatsource/user/UserHeatSourcesConfig.java
@@ -64,7 +64,7 @@ public class UserHeatSourcesConfig extends AbstractUserJSONConfig
 
 	private void addHeatSource(UserHeatSourceEntry entry)
 	{
-		final Block block = Item.REGISTRY.getValue(ResourceLocation);
+		final Block block = GameRegistry.findBlock(entry.mod_id, entry.block_name);
 		if (block != null)
 		{
 			if (entry.states == null || entry.states.size() == 0)
