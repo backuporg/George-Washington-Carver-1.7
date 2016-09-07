eb7cc6a7606d286ff5301b580e35865c605d4f0a
Sunconure11 <eyespot7b@aol.com>
2016-09-07 15:03:29 -0400

    just put the mail over there

--- src/java/growthcraft/api/core/fluids/FluidUtils.java
+++ src/java/growthcraft/api/core/fluids/FluidUtils.java
@@ -13,9 +13,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidBase;
 import net.minecraftforge.fluids.Fluid;
-import net.minecraftforge.fluids.FluidContainerRegistry;
-import net.minecraftforge.fluids.FluidRegistry;
-import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.*;
 import net.minecraftforge.fluids.FluidContainerRegistry.FluidContainerData;
 
 public class FluidUtils
