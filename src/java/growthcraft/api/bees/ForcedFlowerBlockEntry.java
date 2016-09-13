e2ce3e795a7983032a7993efba43989e00204d60
Sunconure11 <eyespot7b@aol.com>
2016-09-13 03:55:17 -0400

    the definitive guide to fishy sticks

--- src/java/growthcraft/api/bees/ForcedFlowerBlockEntry.java
+++ src/java/growthcraft/api/bees/ForcedFlowerBlockEntry.java
@@ -54,7 +54,7 @@ public class ForcedFlowerBlockEntry extends AbstractFlowerBlockEntry
 		if (soilBlock == null) return false;
 		if (getBlockState() instanceof IPlantable)
 		{
-			return soilBlock.canSustainPlant(World, pos.down(), EnumFacing.UP, (IPlantable)getBlockState());
+			return soilBlock.canSustainPlant(world, pos.down(), EnumFacing.UP, (IPlantable)getBlockState());
 		}
 		return true;
 	}
