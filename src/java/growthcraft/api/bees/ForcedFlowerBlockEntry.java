d4c57c15e802911cad77d6748a1f8b120e051870
Sunconure11 <eyespot7b@aol.com>
2016-09-13 03:48:44 -0400

    flowers

--- src/java/growthcraft/api/bees/ForcedFlowerBlockEntry.java
+++ src/java/growthcraft/api/bees/ForcedFlowerBlockEntry.java
@@ -28,6 +28,7 @@ import net.minecraft.world.World;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.IPlantable;
 
 
@@ -53,7 +54,7 @@ public class ForcedFlowerBlockEntry extends AbstractFlowerBlockEntry
 		if (soilBlock == null) return false;
 		if (getBlockState() instanceof IPlantable)
 		{
-			return soilBlock.canSustainPlant(World, pos.down(), EnumFacing.UP, (IPlantable)getBlockState());
+			return soilBlock.canSustainPlant(IBlockState blockState, IBlockAccess world, World world, pos.down(), EnumFacing.UP, (IPlantable)getBlockState());
 		}
 		return true;
 	}
