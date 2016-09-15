3a5fb403c371e05d8c92a576159c111abb72125f
Sunconure11 <eyespot7b@aol.com>
2016-09-15 15:12:15 -0400

    stuff and things

--- src/java/growthcraft/api/core/description/Describer.java
+++ src/java/growthcraft/api/core/description/Describer.java
@@ -36,81 +36,69 @@ import net.minecraft.init.PotionTypes;
 /**
  * A nice way to add descriptions to a list if the Object is an IDescribable
  */
-public class Describer
-{
-	private Describer() {}
+public class Describer {
+    private Describer() {
+    }
 
-	public static void getDescription(List<String> list, Object obj)
-	{
-		if (obj instanceof IDescribable)
-		{
-			((IDescribable)obj).getDescription(list);
-		}
-	}
+    public static void getDescription(List<String> list, Object obj) {
+        if (obj instanceof IDescribable) {
+            ((IDescribable) obj).getDescription(list);
+        }
+    }
 
-	public void getPotionEffectDescription(List<String> list, PotionEffect pe)
-	{
-		if (pe == null) return;
+    public void getPotionEffectDescription(List<String> list, PotionEffect pe) {
+        if (pe == null) return;
 
-		String s = GrcI18n.translate(pe.getEffectName()).trim();
-		final Potion potion = PotionTypes.getPotionTypes[pe.getPotionID()];
-		if (potion != null)
-		{
-			if (potion.isBadEffect())
-				s = TextFormatting.RED + s;
-		}
+        String s = GrcI18n.translate(pe.getEffectName()).trim();
+        final Potion potion = Potion.PotionTypes[pe.getPotionID()];
+        if (potion != null) {
+            if (potion.isBadEffect())
+                s = TextFormatting.RED + s;
+        }
 
-		if (pe.getAmplifier() > 0)
-		{
-			s += " " + GrcI18n.translate("potion.potency." + pe.getAmplifier()).trim();
-		}
+        if (pe.getAmplifier() > 0) {
+            s += " " + GrcI18n.translate("potion.potency." + pe.getAmplifier()).trim();
+        }
 
-		if (pe.potionDuration() > 20)
-		{
-			s += "" + TextFormatting.GRAY + " (" + Potion.getPotionDurationString(pe) + ")";
-		}
-		list.add(s);
-	}
+        if (pe.potionDuration() > 20) {
+            s += "" + TextFormatting.GRAY + " (" + Potion.getPotionDurationString(pe) + ")";
+        }
+        list.add(s);
+    }
 
-	public static void addAllPrefixed(String prefix, List<String> dest, List<String> src)
-	{
-		for (String str : src)
-		{
-			dest.add(prefix + str);
-		}
-	}
+    public static void addAllPrefixed(String prefix, List<String> dest, List<String> src) {
+        for (String str : src) {
+            dest.add(prefix + str);
+        }
+    }
 
-	public static void addAllIndented(List<String> dest, List<String> src)
-	{
-		addAllPrefixed("  ", dest, src);
-	}
+    public static void addAllIndented(List<String> dest, List<String> src) {
+        addAllPrefixed("  ", dest, src);
+    }
 
-	/**
-	 * Attempts to compact the src list into the dest list.
-	 * If the src list only has 1 entry, then it will inline with the head
-	 * else it is treated as an indented list
-	 *
-	 * @param head - str to inline with or use as header
-	 * @param dest - destination list
-	 * @param src - source list
-	 */
-	public static void compactDescription(String head, List<String> dest, List<String> src)
-	{
-		if (src.size() > 0)
-		{
-			if (src.size() == 1)
-			{
-				final String line = src.get(0);
-				dest.add(head + " " + line);
-			}
-			else
-			{
-				dest.add(head);
-				addAllIndented(dest, src);
-			}
-		}
-	}
+    /**
+     * Attempts to compact the src list into the dest list.
+     * If the src list only has 1 entry, then it will inline with the head
+     * else it is treated as an indented list
+     *
+     * @param head - str to inline with or use as header
+     * @param dest - destination list
+     * @param src  - source list
+     */
+    public static void compactDescription(String head, List<String> dest, List<String> src) {
+        if (src.size() > 0) {
+            if (src.size() == 1) {
+                final String line = src.get(0);
+                dest.add(head + " " + line);
+            } else {
+                dest.add(head);
+                addAllIndented(dest, src);
+            }
+        }
+    }
 
 
     public int getAmplifier() {
+        return 0;
+    }
 }
