dd8f244e1b6c5b68a68e21a1990afa2b277aa8f1
Sunconure11 <eyespot7b@aol.com>
2016-09-15 21:36:58 -0400

    fixes

--- src/java/growthcraft/api/core/description/Describer.java
+++ src/java/growthcraft/api/core/description/Describer.java
@@ -61,7 +61,7 @@ public class Describer {
         }
 
         if (pe.getDuration() > 20)  {
-            s += "" + TextFormatting.GRAY + " (" + Potion.getPotionDurationString() + ")";
+            s += "" + TextFormatting.GRAY + " (" + Potion.getPotionDurationString(pe, 1.0F) + ")";
         }
         list.add(s);
     }
