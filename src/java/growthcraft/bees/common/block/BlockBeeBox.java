984249227fcab649900d26f3aaf4d35ef2b4b5a7
Sunconure11 <eyespot7b@aol.com>
2016-09-20 13:13:34 -0400

    trying some shiet

--- src/java/growthcraft/bees/common/block/BlockBeeBox.java
+++ src/java/growthcraft/bees/common/block/BlockBeeBox.java
@@ -13,28 +13,22 @@ import growthcraft.core.util.ItemUtils;
 import net.minecraft.block.SoundType;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.math.AxisAlignedBB;
 
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.*;
 import net.minecraft.block.state.IBlockState;
 
 
-public class BlockBeeBox extends GrcBlockContainer
-{
+public class BlockBeeBox extends GrcBlockContainer {
 	@SideOnly(Side.CLIENT)
 	protected IIcon[] icons;
 
@@ -42,8 +36,7 @@ public class BlockBeeBox extends GrcBlockContainer
 	private int flammability;
 	private int fireSpreadSpeed;
 
-	public BlockBeeBox(Material material)
-	{
+	public BlockBeeBox(Material material) {
 		super(material);
 		setBlockTextureName("grcbees:beebox");
 		setTickRandomly(true);
@@ -54,314 +47,285 @@ public class BlockBeeBox extends GrcBlockContainer
 		setTileEntityType(TileEntityBeeBox.class);
 	}
 
-	public BlockBeeBox()
-	{
+	public BlockBeeBox() {
 		this(Material.WOOD);
 	}
 
-	public String getMetaname(int meta)
-	{
-		if (meta >= 0 && meta < EnumMinecraftWoodType.VALUES.length)
-		{
+	public String getMetaname(int meta) {
+		if (meta >= 0 && meta < EnumMinecraftWoodType.VALUES.length) {
 			return EnumMinecraftWoodType.VALUES[meta].name;
 		}
 		return "" + meta;
 	}
 
-	public BlockBeeBox setFlammability(int flam)
-	{
+	public BlockBeeBox setFlammability(int flam) {
 		this.flammability = flam;
 		return this;
 	}
 
-	public BlockBeeBox setFireSpreadSpeed(int speed)
-	{
+	public BlockBeeBox setFireSpreadSpeed(int speed) {
 		this.fireSpreadSpeed = speed;
 		return this;
 	}
 
 	@Override
-	public int getFlammability(IBlockAccess world, int x, int y, int z, EnumFacing face)
-	{
+	public int getFlammability(IBlockAccess world, int x, int y, int z, EnumFacing face) {
 		return flammability;
 	}
 
 	@Override
-	public int getFireSpreadSpeed(IBlockAccess world, int x, int y, int z, EnumFacing face)
-	{
+	public int getFireSpreadSpeed(IBlockAccess world, int x, int y, int z, EnumFacing face) {
 		return fireSpreadSpeed;
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	@SuppressWarnings({"rawtypes", "unchecked"})
-	public void getSubBlocks(Item block, CreativeTabs tab, List list)
-	{
-		for (EnumMinecraftWoodType woodType : EnumMinecraftWoodType.VALUES)
-		{
+	public void getSubBlocks(Item block, CreativeTabs tab, List list) {
+		for (EnumMinecraftWoodType woodType : EnumMinecraftWoodType.VALUES) {
 			list.add(new ItemStack(block, 1, woodType.meta));
 		}
 	}
 
 	@Override
-	public void updateTick(World world, BlockPos pos, Random rand, IBlockState state)
-	{
+	public void updateTick(World world, BlockPos pos, Random rand, IBlockState state) {
 		super.updateTick(world, pos, state, rand);
 		final TileEntityBeeBox te = getTileEntity(world, pos);
 		if (te != null) te.updateBlockTick();
 	}
 
-	@Override
 	@SideOnly(Side.CLIENT)
-	public void randomDisplayTick(World world, BlockPos pos, Random rand)
-	{
-		if (rand.nextInt(24) == 0)
-		{
-			final TileEntityBeeBox te = (TileEntityBeeBox)world.getTileEntity(pos);
-			if (te != null)
-			{
-				if (te.hasBees())
-				{
-					if (te.hasBees())
-					{
+	public int randomDisplayTick(World world, BlockPos pos, Random rand) {
+		if (rand.nextInt(24) == 0) {
+			final TileEntityBeeBox te = (TileEntityBeeBox) world.getTileEntity(pos);
+			if (te != null) {
+				if (te.hasBees()) {
+					if (te.hasBees()) {
 						world.playSound(
-								(double)pos.getX() + 0.5D,
-								(double)pos.getY() + 0.5D,
-								(double)pos.getZ() + 0.5D,
+								(double) pos.getX() + 0.5D,
+								(double) pos.getY() + 0.5D,
+								(double) pos.getZ() + 0.5D,
 								"grcbees:buzz",
 								1.0F + rand.nextFloat(),
 								0.3F + rand.nextFloat() * 0.7F,
 								false);
+					}
 				}
 			}
 		}
-	}
 
-	/************
-	 * TRIGGERS
-	 ************/
+		/************
+		 * TRIGGERS
+		 ************/
 		@Override
-		public boolean onBlockActivated(World world, BlockPos pos, EntityPlayer player, int meta, float par7, float par8, float par9)
-	    {
-		if (super.onBlockActivated(world, pos, player, meta, par7, par8, par9)) return true;
-		if (world.isRemote)
+		public boolean onBlockActivated (World world, BlockPos pos, EntityPlayer player,int meta, float par7,
+		float par8, float par9)
+		{
+			if (super.onBlockActivated(world, pos, player, meta, par7, par8, par9)) return true;
+			if (world.isRemote) {
+				return true;
+			} else {
+				final TileEntityBeeBox te = (TileEntityBeeBox) world.getTileEntity(pos);
+				if (te != null) {
+					player.openGui(GrowthCraftBees.instance, 0, world, pos);
+					return true;
+				}
+				return false;
+			}
+		}
+
+		@Override
+		public void breakBlock (World world, BlockPos pos, IBlockState block,int par5)
+		{
+			final TileEntityBeeBox te = (TileEntityBeeBox) world.getTileEntity(pos);
+
+			if (te != null) {
+				for (int index = 0; index < te.getSizeInventory(); ++index) {
+					final ItemStack stack = te.getStackInSlot(index);
+
+					ItemUtils.spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(), stack, rand);
+				}
+
+				world.updateComparatorOutputLevel(pos, par5);
+			}
+
+			super.breakBlock(world, pos);
+		}
+
+		/************
+		 * CONDITIONS
+		 ************/
+		@Override
+		public boolean isSideSolid (IBlockAccess world, BlockPos pos, EnumFacing side)
+		{
+			return EnumFacing.UP == side;
+		}
+
+		@Override
+		public TileEntity createNewTileEntity (World world,int par2)
+		{
+			return new TileEntityBeeBox();
+		}
+
+		/************
+		 * DROPS
+		 ************/
+		@Override
+		public int damageDropped ( int damage)
+		{
+			return damage;
+		}
+
+		@Override
+		public int quantityDropped (Random random)
+		{
+			return 1;
+		}
+
+		/************
+		 * TEXTURES
+		 ************/
+		@SideOnly(Side.CLIENT)
+		protected void registerBeeBoxIcons (IIconRegister reg, String basename,int offset)
+		{
+			icons[offset * 4] = reg.registerIcon(getTextureName() + basename + "bottom");
+			icons[offset * 4 + 1] = reg.registerIcon(getTextureName() + basename + "top");
+			icons[offset * 4 + 2] = reg.registerIcon(getTextureName() + basename + "side");
+			icons[offset * 4 + 3] = reg.registerIcon(getTextureName() + basename + "side_honey");
+		}
+
+		@Override
+		@SideOnly(Side.CLIENT)
+		public void registerBlockIcons (IIconRegister reg)
+		{
+			this.icons = new IIcon[6 * 4];
+
+			for (EnumMinecraftWoodType woodType : EnumMinecraftWoodType.VALUES) {
+				registerBeeBoxIcons(reg, String.format("/minecraft/%s/", woodType.name), woodType.meta);
+			}
+		}
+
+		@SideOnly(Side.CLIENT)
+		protected int calculateIconOffset ( int meta)
+		{
+			return MathHelper.clamp_int(meta, 0, icons.length / 4 - 1) * 4;
+		}
+
+		@Override
+		@SideOnly(Side.CLIENT)
+		public IIcon getIcon (IBlockAccess world, BlockPos pos,int side);
+		{
+			final int meta = world.getBlockState(pos);
+			final int offset = calculateIconOffset(meta);
+			if (side == 0) {
+				return icons[offset];
+			} else if (side == 1) {
+				return icons[offset + 1];
+			} else {
+				final TileEntityBeeBox te = (TileEntityBeeBox) world.getTileEntity(x, y, z);
+				if (te != null && te.isHoneyEnough(6)) {
+					return icons[offset + 3];
+				}
+			}
+			return icons[offset + 2];
+		}
+
+		@Override
+		@SideOnly(Side.CLIENT)
+		public IIcon getIcon ( int side, int meta)
+		{
+			final int offset = calculateIconOffset(meta);
+			if (side == 0) {
+				return icons[offset];
+			} else if (side == 1) {
+				return icons[offset + 1];
+			}
+			return icons[offset + 2];
+		}
+
+		@SideOnly(Side.CLIENT)
+		public IIcon[] getIcons ()
+		{
+			return icons;
+		}
+
+		/************
+		 * RENDERS
+		 ************/
+		@Override
+		public int getRenderType ()
+		{
+			return RenderBeeBox.id;
+		}
+
+		@Override
+		public boolean isOpaqueCube ()
+		{
+			return false;
+		}
+
+		@Override
+		public boolean renderAsNormalBlock ()
+		{
+			return false;
+		}
+
+		@Override
+		@SideOnly(Side.CLIENT)
+		public boolean shouldSideBeRendered (IBlockAccess world, BlockPos pos,int side)
 		{
 			return true;
 		}
-		else
+
+		/************
+		 * BOXES
+		 ************/
+		@Override
+		public void setBlockBoundsForItemRender ()
 		{
-			final TileEntityBeeBox te = (TileEntityBeeBox)world.getTileEntity(pos);
-			if (te != null)
-			{
-				player.openGui(GrowthCraftBees.instance, 0, world, pos);
-				return true;
-			}
-			return false;
+			setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
 		}
-	}
-
-	@Override
-	public void breakBlock(World world, BlockPos pos, IBlockState block, int par5)
-	{
-		final TileEntityBeeBox te = (TileEntityBeeBox)world.getTileEntity(pos);
-
-		if (te != null)
-		{
-			for (int index = 0; index < te.getSizeInventory(); ++index)
-			{
-				final ItemStack stack = te.getStackInSlot(index);
-
-				ItemUtils.spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(), stack, rand);
-			}
-
-			world.updateComparatorOutputLevel(pos, par5);
-		}
-
-		super.breakBlock(world, pos);
-	}
-
-	/************
-	 * CONDITIONS
-	 ************/
-	@Override
-	public boolean isSideSolid(IBlockAccess world, BlockPos pos, EnumFacing side)
-	{
-		return EnumFacing.UP == side;
-	}
-
-	@Override
-	public TileEntity createNewTileEntity(World world, int par2)
-	{
-		return new TileEntityBeeBox();
-	}
-
-	/************
-	 * DROPS
-	 ************/
-	@Override
-	public int damageDropped(int damage)
-	{
-		return damage;
-	}
-
-	@Override
-	public int quantityDropped(Random random)
-	{
-		return 1;
-	}
-
-	/************
-	 * TEXTURES
-	 ************/
-	@SideOnly(Side.CLIENT)
-	protected void registerBeeBoxIcons(IIconRegister reg, String basename, int offset)
-	{
-		icons[offset * 4] = reg.registerIcon(getTextureName() + basename + "bottom");
-		icons[offset * 4 + 1] = reg.registerIcon(getTextureName() + basename + "top");
-		icons[offset * 4 + 2] = reg.registerIcon(getTextureName() + basename + "side");
-		icons[offset * 4 + 3] = reg.registerIcon(getTextureName() + basename + "side_honey");
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void registerBlockIcons(IIconRegister reg)
-	{
-		this.icons = new IIcon[6 * 4];
-
-		for (EnumMinecraftWoodType woodType : EnumMinecraftWoodType.VALUES)
-		{
-			registerBeeBoxIcons(reg, String.format("/minecraft/%s/", woodType.name), woodType.meta);
-		}
-	}
-
-	@SideOnly(Side.CLIENT)
-	protected int calculateIconOffset(int meta)
-	{
-		return MathHelper.clamp_int(meta, 0, icons.length / 4 - 1) * 4;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public IIcon getIcon(IBlockAccess world, BlockPos pos, int side);
-	{
-		final int meta = world.getBlockState(pos);
-		final int offset = calculateIconOffset(meta);
-		if (side == 0)
-		{
-			return icons[offset];
-		}
-		else if (side == 1)
-		{
-			return icons[offset + 1];
-		}
-		else
-		{
-			final TileEntityBeeBox te = (TileEntityBeeBox)world.getTileEntity(x, y, z);
-			if (te != null && te.isHoneyEnough(6))
-			{
-				return icons[offset + 3];
-			}
-		}
-		return icons[offset + 2];
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public IIcon getIcon(int side, int meta)
-	{
-		final int offset = calculateIconOffset(meta);
-		if (side == 0)
-		{
-			return icons[offset];
-		}
-		else if (side == 1)
-		{
-			return icons[offset + 1];
-		}
-		return icons[offset + 2];
-	}
-
-	@SideOnly(Side.CLIENT)
-	public IIcon[] getIcons()
-	{
-		return icons;
-	}
-
-	/************
-	 * RENDERS
-	 ************/
-	@Override
-	public int getRenderType()
-	{
-		return RenderBeeBox.id;
-	}
-
-	@Override
-	public boolean isOpaqueCube()
-	{
-		return false;
-	}
-
-	@Override
-	public boolean renderAsNormalBlock()
-	{
-		return false;
-	}
-
-	@Override
-	@SideOnly(Side.CLIENT)
-	public boolean shouldSideBeRendered(IBlockAccess world, BlockPos pos, int side)
-	{
-		return true;
-	}
-
-	/************
-	 * BOXES
-	 ************/
-	@Override
-	public void setBlockBoundsForItemRender()
-	{
-		setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
-	}
 
 		@Override
 		@SuppressWarnings({"rawtypes", "unchecked"})
-		public void addCollisionBoxesToList(World world, BlockPos pos, IBlockState state, AxisAlignedBB axis, List list, Entity entity)
+		public void addCollisionBoxesToList (World world, BlockPos pos, IBlockState state, AxisAlignedBB axis, List
+		list, Entity entity)
 		{
 			final float f = 0.0625F;
 			// LEGS
-			setBlockBounds(3*f, 0.0F, 3*f, 5*f, 3*f, 5*f);
+			setBlockBounds(3 * f, 0.0F, 3 * f, 5 * f, 3 * f, 5 * f);
 			super.addCollisionBoxesToList(world, pos, state, axis, list, entity);
-			setBlockBounds(11*f, 0.0F, 3*f, 13*f, 3*f, 5*f);
+			setBlockBounds(11 * f, 0.0F, 3 * f, 13 * f, 3 * f, 5 * f);
 			super.addCollisionBoxesToList(world, pos, state, axis, list, entity);
-			setBlockBounds(3*f, 0.0F, 11*f, 5*f, 3*f, 13*f);
+			setBlockBounds(3 * f, 0.0F, 11 * f, 5 * f, 3 * f, 13 * f);
 			super.addCollisionBoxesToList(world, pos, state, axis, list, entity);
-			setBlockBounds(11*f, 0.0F, 11*f, 13*f, 3*f, 13*f);
+			setBlockBounds(11 * f, 0.0F, 11 * f, 13 * f, 3 * f, 13 * f);
 			super.addCollisionBoxesToList(world, pos, state, axis, list, entity);
 			// BODY
-			setBlockBounds(1*f, 3*f, 1*f, 15*f, 10*f, 15*f);
+			setBlockBounds(1 * f, 3 * f, 1 * f, 15 * f, 10 * f, 15 * f);
 			super.addCollisionBoxesToList(world, pos, state, axis, list, entity);
 			// ROOF
-			setBlockBounds(0.0F, 10*f, 0.0F, 1.0F, 13*f, 1.0F);
+			setBlockBounds(0.0F, 10 * f, 0.0F, 1.0F, 13 * f, 1.0F);
 			super.addCollisionBoxesToList(world, pos, state, axis, list, entity);
-			setBlockBounds(2*f, 13*f, 2*f, 14*f, 1.0F, 14*f);
+			setBlockBounds(2 * f, 13 * f, 2 * f, 14 * f, 1.0F, 14 * f);
 			super.addCollisionBoxesToList(world, pos, state, axis, list, entity);
 			setBlockBoundsForItemRender();
 		}
 
-	/************
-	 * COMPARATOR
-	 ************/
-	@Override
-	public boolean hasComparatorInputOverride()
-	{
-		return true;
-	}
+		/************
+		 * COMPARATOR
+		 ************/
+		@Override
+		public boolean hasComparatorInputOverride ()
+		{
+			return true;
+		}
 
-	@Override
-	public int getComparatorInputOverride(World world, BlockPos pos, int par5)
-	{
-		final TileEntityBeeBox te = (TileEntityBeeBox)world.getTileEntity(pos);
-		return te.countHoney() * 15 / 27;
+		@Override
+		public int getComparatorInputOverride (World world, BlockPos pos,int par5)
+		{
+			final TileEntityBeeBox te = (TileEntityBeeBox) world.getTileEntity(pos);
+			return te.countHoney() * 15 / 27;
+		}
 	}
 }
