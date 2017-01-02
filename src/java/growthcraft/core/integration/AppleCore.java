f6049580bb96156ad01f81398cc0c38544bf1dbb
Sunconure11 <eyespot7b@aol.com>
2017-01-01 21:21:09 -0500

    fixes

--- src/java/growthcraft/core/integration/AppleCore.java
+++ src/java/growthcraft/core/integration/AppleCore.java
@@ -2,6 +2,8 @@ package growthcraft.core.integration;
 
 import growthcraft.core.GrowthCraftCore;
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Optional;
@@ -28,29 +30,29 @@ public class AppleCore extends ModIntegrationBase
 
 	// abstract the AppleCoreAPI reference into an Optional.Method so that AppleCore is not a hard dependency
 	@Optional.Method(modid=MOD_ID)
-	private static Event.Result validateGrowthTick_AC(Block block, World world, BlockPos pos, Random random)
+	private static Event.Result validateGrowthTick_AC(Block block, World world, BlockPos pos, Random random, IBlockState state)
 	{
-		return AppleCoreAPI.dispatcher.validatePlantGrowth(block, world, x, y, z, random);
+		return AppleCoreAPI.dispatcher.validatePlantGrowth(block, world, pos, state, random);
 	}
 
-	public static Event.Result validateGrowthTick(Block block, World world, BlockPos pos, Random random)
+	public static Event.Result validateGrowthTick(Block block, World world, BlockPos pos, Random random, IBlockState state)
 	{
 		if (appleCoreLoaded)
-			return validateGrowthTick_AC(block, world, x, y, z, random);
+			return validateGrowthTick_AC(block, world, pos, random, state);
 
 		return Event.Result.DEFAULT;
 	}
 
 	// abstract the AppleCoreAPI reference into an Optional.Method so that AppleCore is not a hard dependency
 	@Optional.Method(modid=MOD_ID)
-	private static void announceGrowthTick_AC(Block block, World world, BlockPos pos, int previousMetadata)
+	private static void announceGrowthTick_AC(Block block, World world, BlockPos pos, int previousMetadata, IBlockState state)
 	{
-		AppleCoreAPI.dispatcher.announcePlantGrowth(block, world, x, y, z, previousMetadata);
+		AppleCoreAPI.dispatcher.announcePlantGrowth(block, world, pos, state);
 	}
 
-	public static void announceGrowthTick(Block block, World world, BlockPos pos, int previousMetadata)
+	public static void announceGrowthTick(Block block, World world, BlockPos pos, int previousMetadata, IBlockState state)
 	{
 		if (appleCoreLoaded)
-			announceGrowthTick_AC(block, world, x, y, z, previousMetadata);
+			announceGrowthTick_AC(block, world, pos, previousMetadata, state);
 	}
 }
--- src/java/growthcraft/hops/client/renderer/RenderHops.java
+++ src/java/growthcraft/hops/client/renderer/RenderHops.java
@@ -5,6 +5,7 @@ import growthcraft.hops.common.block.BlockHops;
 import net.minecraft.block.Block;
 
 import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
